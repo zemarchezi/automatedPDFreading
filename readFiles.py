772601ae33b191556b2781ce9082dce29badf90b
Jose Paulo Marchezi <jpmarchezi@gmail.com>
2022-04-25 11:52:05 -0300

    readding first

--- readFiles.py
+++ readFiles.py
@@ -25,8 +25,13 @@ filename = 'Sumário(06:12-13:12)_Laysa.docx'
 z = zipfile.ZipFile(PATH+filename)
 all_files = z.namelist()
 images = [x for x in all_files if x.startswith('word/media/')]
-
-
+#%%
+def saveFigs(zipimgpath, outputpath, figname):
+    image1 = z.open(zipimgpath).read()
+    f = open(f'{outputpath}/{figname}.png','wb')
+    f.write(image1)
+    return f'{outputpath}/{figname}.png'
+#%%
 def get_bold_list(para):
     bold_list = []
     for run in para.runs:
@@ -63,6 +68,28 @@ for nnb, bol in enumerate(bolds):
                 dictsposition[bol]['pt'] = nn
             if dictionaryEng.check(texst[nn+1].split(' ')[0]):
                 dictsposition[bol]['en'] = nn
+
+
+#%%
+keys = list(dictsposition.keys())
+textpt = ''
+texten = ''
+for i in range(len(keys)):
+    figname = keys[i].replace(' ', '').replace(':','')
+    outfigpath = saveFigs(dictsposition[keys[i]]['img'], f"./figures", figname)
+    textpt += f'{keys[i]}\n'
+    texten += f'{keys[i]}\n'
+    if i+1 < len(keys):
+        texTemppt = '\n'.join(texst[dictsposition[keys[i]]['pt']+1:dictsposition[keys[i+1]]['pt']])
+        texTempen = '\n'.join(texst[dictsposition[keys[i]]['en']+1:dictsposition[keys[i+1]]['en']])
+    else:
+        texTemppt = '\n'.join(texst[dictsposition[keys[i]]['pt']+1:dictsposition[keys[0]]['en']])
+        texTempen = '\n'.join(texst[dictsposition[keys[i]]['en']+1:])
+
+    textpt += f'{texTemppt}\n'
+    textpt += f"{outfigpath}\n"
+    texten += f'{texTempen}\n'
+    texten += f"{outfigpath}\n"
 
 # %%
 image1 = z.open('word/media/image1.png').read()
