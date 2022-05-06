c4adf7e4435f37c5481edfb3ee361be1142231a5
Jose P Marchezi <jpmarchezi@gmail.com>
2022-05-06 17:23:02 -0300

    download data

--- readFiles_ROTI.py
+++ readFiles_ROTI.py
@@ -1,98 +1,102 @@
 #%%
 from PIL import Image
-import numpy as np
-from io import BytesIO
-import fitz
-from matplotlib import image
-from generateLatexFile import *
-from pdf2image import convert_from_path
-from polyglot.detect import Detector
-import cv2
+from io import StringIO
 import matplotlib.pyplot as plt
-import re
-#%%
+from docx import Document
+import zipfile
+from polyglot.detect import Detector
+from generateLatexFile import *
+import cv2
+import numpy as np
 
-def extractItemize_Scint(text):
-    texs = """\\begin{itemize} \n """
-    for i in text.split('•'):
-        if len(i) > 1:
-            texs += '\\item ' + ' '.join(i.split('\n')) +'\n'
-            if i.startswith('TEC'):
-                texs += '\\subsectoin{%s}' %(i) +'\n'
-    
-    texs += """\\end{itemize} \n """
-
-    return texs
-
-
-def saveFigs(pages, page, outputpath, figname, crop):
-    with BytesIO() as image_byte_array:
-        pages[page].save(image_byte_array, format='PNG')
-        image_to_extract = image_byte_array.getvalue()
-        img = cv2.imdecode(np.frombuffer(image_to_extract, np.uint8),3)
+# %%
+def saveFigs(zipf, zipimgpath, outputpath, figname, crop):
+    image1 = zipf.open(zipimgpath).read()
+    img = cv2.imdecode(np.frombuffer(image1, np.uint8),3)
+    if crop:
         imgCropped = img[crop[0]:crop[1],crop[2]:crop[3]]
+    else:
+        imgCropped = img
     cv2.imwrite(f'{outputpath}/{figname}.png',imgCropped)
     return f'{outputpath}/{figname}.png'
 
-#%%
-def extractFiguresTextScint(docPath, filename, outputimage):
+def get_bold_list(para):
+    bold_list = []
+    for run in para.runs:
+        bold_list.append(run.bold)
+    return bold_list
 
-    pages = convert_from_path(docPath + filename, 500)
+def extractFiguresTextRadBelts(docPath, filename):
+
+    z = zipfile.ZipFile(docPath+filename)
+    all_files = z.namelist()
+    images = [x for x in all_files if x.startswith('word/media/')]
+
+    document = Document(docPath+filename)
+    texs= []
+    for para in document.paragraphs:
+        texs.append(para.text)
+
+    dictsposition = {}
+    fig = 0
+    for n, i in enumerate(texs):
+        if i.startswith('Fig'):
+            fig += 1
+            dictsposition[f"Figure_{fig}"] = {'leg': i,
+                                              'fig': images[fig-1]}
+        if i.startswith('Summary') or i.startswith('Resumo'):
+            dictsposition[f"locSummary"] = n
+
+    return dictsposition, texs, z
 
 
-    textDict = {"Tabela": {'page': 0, 
-                              'crop': [1500,4000,250,3800]},
-                  "imager": {'page': 1,
-                              'regex_en': r"(?<=Remarks.)(.|\n)*",
-                              'regex_pt': r"(?<=Observacoes.)(.|\n)*(?=Remarks)"},
-                  "tec": {'page': 2,
-                              'regex_en': r"(?<=Remarks.)(.|\n)*",
-                              'regex_pt': r"(?<=Observacoes.)(.|\n)*(?=Remarks)"}
-                  }
+def constructLatexFileRadBelts(docPath, filename, outputFigure):
+    dictsposition, texst, zipf = extractFiguresTextRadBelts(docPath, filename)
+    keys = list(dictsposition.keys())
+    detector = Detector(dictsposition[keys[1]]['leg'])
+    if detector.language.code == 'pt':
+        text = '\section{ROTI} \n \subsection{Responsável: Carolina de Sousa do Carmo} \n \n'
+    if detector.language.code == 'en':
+        text = '\section{ROTI} \n \subsection{Responsible: Carolina de Sousa do Carmo} \n \n'
+    keys = list(dictsposition.keys())
+    
     includeFigure = """\\begin{figure}[H]\n    
                         \\centering\n   
                              \\includegraphics[width=14cm]{./%s}\n
+                             \\caption{%s}
                         \\end{figure}\n
                      """
-    keys = list(textDict.keys())
-    ttextEn = ''
-    ttextPt = ''
-    # ttextPt = '\section{Imageador All-Sky} \n \subsection{Responsável: LUME} \n \n'
-    # ttextEn = '\section{All-Sky Imager} \n \subsection{Responsible: LUME} \n \n' 
 
     
     for i in range(len(keys)):
-        if 'regex_en' not in list(textDict[keys[i]].keys()):
-            crops = textDict[keys[i]]['crop']
-            outfigpath = saveFigs(pages, textDict[keys[i]]['page'], 
-                                f"{outputimage}", f'figureScint_{i}', 
-                                crops)
-            # includeFigure % ('/'.join(outfigpath.split('/')[2:]))
-        else:
-            if keys[i] == 'tec':
-                ttextPt += 'TEC \n'
-                ttextEn += 'TEC \n'
-            doc = fitz.open(docPath + filename)
-            text = doc[textDict[keys[i]]['page']].get_text()
-            text = re.sub('[\¸\˜\´\❘❙❚]+', '', text)
-            matches = re.search(textDict[keys[i]]['regex_en'], text, re.MULTILINE)
-            
-            if matches:
-                ttextEn += extractItemize_Scint(matches.group())
-            matches = re.search(textDict[keys[i]]['regex_pt'], text, re.MULTILINE)
-            if matches:
-                ttextPt += extractItemize_Scint(matches.group())
+        if keys[i].startswith("Fig"):
 
-    return ttextEn, ttextPt
+            if keys[i].endswith("1"):
+                crops = False
+            if keys[i].endswith("2"):
+                crops = False
+            outfigpath = saveFigs(zipf, dictsposition[keys[i]]['fig'], f"{outputFigure}", f'figureRadBelts_{i}', crops)
 
-#%%
+            text += includeFigure % ('/'.join(outfigpath.split('/')[2:]), dictsposition[keys[i]]['leg'])
+
+    
+    textSum = '\n\n'.join(texst[dictsposition['locSummary']+1:])
+    text += textSum
+
+
+    return text
+
+
 
 PATH = './data/'
 
-filename = 'EMBRACE_BRIEFING_REPORT_24.04-30.04_Prosper.pdf'
+filename = 'Resumo_semana2207_Carolina.docx'
 
 #%%
-texten, textpt = extractFiguresTextScint(PATH, filename, outputimage='./latexText/figures/')
-generateLaTexFile(textpt, EnPt=True, outputPath='./latexText', date='2022/04/25')
+# ssa = extractFiguresTextRadBelts(PATH, filename)
+ssa = constructLatexFileRadBelts(PATH, filename, outputFigure='./latexText/figures')
 
-#%%
+generateLaTexFile(ssa, EnPt=False, outputPath='./latexText', date='25/04/2022')
+# %%
+
+# %%
